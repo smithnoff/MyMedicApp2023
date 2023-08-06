ed02fe6bc0ab3de520df924000a53ca7bbcad4c9
Eillen <gonzalezeillendelvalle@gmail.com>
2023-08-06 10:59:59 -0500

    commitviewmodel

---
+++ app/src/main/java/com/skynoff/mymedicapp/Register/RegisterViewModel.kt
@@ -0,0 +1,22 @@
+package com.skynoff.mymedicapp.Register
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class RegisterViewModel : ViewModel() {
+    private val auth: FirebaseAuth = Firebase.auth
+    val isUserRegister = MutableLiveData<Boolean>()
+
+    fun registrar(email:String,password:String){
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            isUserRegister.postValue(task.isSuccessful)
+        }
+
+    }
+
+
+
+}
