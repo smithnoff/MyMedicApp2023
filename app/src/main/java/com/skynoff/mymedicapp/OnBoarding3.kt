daa7003eef7b82d1635a337eef240e60bf5e9ca6
KilloDev <127050422+KilloDev@users.noreply.github.com>
2023-08-12 19:06:57 -0400

    Creacion de la pantalla 3 de OnBoarding

---
+++ app/src/main/java/com/skynoff/mymedicapp/OnBoarding3.kt
@@ -0,0 +1,11 @@
+package com.skynoff.mymedicapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnBoarding3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding3)
+    }
+}
--- app/src/main/java/com/skynoff/mymedicapp/login/LoginActivity.kt
+++ app/src/main/java/com/skynoff/mymedicapp/login/LoginActivity.kt
@@ -1,46 +1,102 @@
 package com.skynoff.mymedicapp.login
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ClickableSpan
+import android.util.Patterns
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
-import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.textfield.TextInputEditText
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import androidx.appcompat.app.AppCompatActivity
 import com.skynoff.mymedicapp.R
-import com.skynoff.mymedicapp.databinding.ActivityLoginBinding
-import com.skynoff.mymedicapp.home.MainActivity
+import com.skynoff.mymedicapp.joined.JoinedActivity
+import com.skynoff.mymedicapp.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: LoginViewModel
-    private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        initViews()
-        initObservables()
-    }
-    private fun initViews() {
-        viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
-        binding.button.setOnClickListener {
-             val email = binding.etUserEmail.text.toString().lowercase().trim()
-             val pass = binding.etUserPassword.text.toString()
-            viewModel.loginUser(email,pass)
-        }
-    }
-    private fun initObservables() {
+        setContentView(R.layout.activity_login)
 
-        viewModel.isUserLogged.observe(this){ isLogged ->
-            if(isLogged){
-                startActivity(Intent(this,MainActivity::class.java))
-            }else{
-                Toast.makeText(this, "Usuario o contraseña invalidos", Toast.LENGTH_SHORT).show()
+
+        val email = findViewById<EditText>(R.id.etEmail)
+        val pass = findViewById<EditText>(R.id.etPassword)
+        val btnIngresar = findViewById<Button>(R.id.buttonLogin)
+
+        btnIngresar.setOnClickListener {
+
+             if (email.text.toString().isEmpty()){
+                email.error = "Debe escribir un Email valido"
+                return@setOnClickListener
+
+            }
+            else if (pass.text.toString().isEmpty()){
+                pass.error = "Debe escribir una contraseña"
+                return@setOnClickListener
+
+            }else if(isValidEmail(email.text.toString().lowercase().trim()) && isValidPassword(pass.text.toString())){
+            validateUser(email.text.toString().lowercase().trim(), pass.text.toString())
+        }
+            else{
+                Toast.makeText(this, "Clave o contraseña incorrecta", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+      val linkTextView = findViewById<TextView>(R.id.textRegister)
+        val text = linkTextView.text.toString()
+
+        val linkStartIndex = text.indexOf("Registrate")
+        val linkEndIndex = linkStartIndex + "Registrate".length
+
+        val spannableString = SpannableString(text)
+        val clickableSpan = object : ClickableSpan(){
+            override fun onClick(widget: View) {
             }
         }
+
+spannableString.setSpan(clickableSpan, linkStartIndex, linkEndIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        linkTextView.text = spannableString
+        linkTextView.movementMethod = android.text.method.LinkMovementMethod.getInstance()
+
+
+        linkTextView.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+    private fun validateUser(Email: String, pass: String) {
+        val intent = Intent(this, JoinedActivity::class.java)
+        startActivity(intent)
+    }
+    private fun isValidEmail(email:String): Boolean{
+        val regex = Patterns.EMAIL_ADDRESS.pattern()
+        val pattern = Regex(regex)
+        return pattern.matches(email)
+    }
+    private fun isValidPassword(password: String): Boolean {
+        var hasUpperCase = false
+        var hasNumber = false
+        var hasLowerCase = false
+
+        for (char in password) {
+            if (char.isUpperCase()) {
+                hasUpperCase = true
+            } else if (char.isDigit()) {
+                hasNumber = true
+            }
+            else if (char.isLowerCase()){
+                hasLowerCase = true
+            }
+            if (hasUpperCase && hasNumber && hasLowerCase) {
+                break
+            }
+        }
+        return hasUpperCase && hasNumber && hasLowerCase && password.length >= 8
+}
 }
