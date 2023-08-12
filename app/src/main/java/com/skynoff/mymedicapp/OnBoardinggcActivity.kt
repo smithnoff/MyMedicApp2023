475a511aff2d1bbc8fdec175324c6cb048103839
Eillen <gonzalezeillendelvalle@gmail.com>
2023-08-12 09:10:15 -0500

    Onboardingtwo

---
+++ app/src/main/java/com/skynoff/mymedicapp/OnBoardinggcActivity.kt
@@ -0,0 +1,12 @@
+package com.skynoff.mymedicapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.skynoff.mymedicapp.R
+
+class OnBoardinggcActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boardinggc)
+    }
+}
