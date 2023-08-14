e775cee93a9371aee3c1de13b61ec20cd287ac46
Eillen <gonzalezeillendelvalle@gmail.com>
2023-08-14 08:58:05 -0500

    colorbutton

--- app/src/main/java/com/skynoff/mymedicapp/signup/RegisterViewModel.kt
+++ app/src/main/java/com/skynoff/mymedicapp/signup/RegisterViewModel.kt
@@ -1,22 +1,23 @@
 package com.skynoff.mymedicapp.signup
 
+import android.content.Intent
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.skynoff.mymedicapp.login.LoginActivity
 
 class RegisterViewModel : ViewModel() {
     private val auth: FirebaseAuth = Firebase.auth
     val isUserRegister = MutableLiveData<Boolean>()
 
-    fun registrar(email:String,password:String){
-        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+    fun registrar(email:String,password:String) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
             isUserRegister.postValue(task.isSuccessful)
         }
 
+
     }
-
-
-
 }
